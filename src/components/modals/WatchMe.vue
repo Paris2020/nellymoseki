1faf24d07adc6fbcd1049150a62b0eb61b5de985
Paris2020 <nelly.moseki@amazeelabs.com>
2023-08-10 11:32:18 +0200

    Feature - Initial setup of modal pop up windwo

---
+++ src/components/modals/WatchMe.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import {computed, reactive} from "vue";
+
+    const props = defineProps('modal');
+    const state = reactive({
+        isClosed: false
+    });
+
+    // METHODS
+    const close = () => {
+        state.isClosed = true;
+
+        setTimeout(() => {
+
+            store.commit('updateModalVisibility', false);
+            state.isClosed = false;
+
+        }, 300);
+    }
+
+    //COMPUTED
+    const isVisible = computed(() => {
+        return store.getters['modalIsVisible'];
+    });
+            
+</script>
+
+<template>
+    <div class="modal-holder" :class="{ 'is-closed': state.isClosed }" v-if="isVisible">
+        <div class="modal-backdrop" @click="close()"></div>
+
+        <div class="modal">
+            <div class="modal-close" @click="close()"><span></span></div>
+            <div class="modal-body">
+                <div class="modal-left">
+                    <h2>Video coming soon...</h2>
+                </div> <!-- end.modal-left -->
+            </div> <!-- end.modal-body -->
+        </div> <!-- end.modal -->
+    </div> <!-- end.modal-holder -->
+</template>
+

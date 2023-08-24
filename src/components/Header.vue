fcc3dfc130e54a6a00a039baa38a2ad1b4896632
Paris2020 <nelly.moseki@amazeelabs.com>
2023-08-24 16:12:50 +0200

    Feature - Update header content

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@
                 <img src="/images/profile.png" width="250" alt="Photo by @siede.lee">
             </div>
 
-            <h4>Salut! My name is Nelly &#128081</h4>
+            <h4>Salut! I'm Nelly &#128081 <br />Founder: <strong><a href="https://www.mosekiwebagency.co.za" target="_blank">Moseki Web Agency</a></strong></h4>
 
             <h1>Building digital<br />products, brands and<br />experience.</h1>
             <a href="#" class="btn large unclear" @click="openModal">Watch me!</a>

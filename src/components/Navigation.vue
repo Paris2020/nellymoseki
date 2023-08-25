c5753d346e198b975ee415b243538b3c5a4c5761
Paris2020 <nelly.moseki@amazeelabs.com>
2023-08-25 19:53:20 +0200

    feature - update email address and image name

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,12 +5,12 @@
 <template>
     <nav class="navigation">
         <div class="wrapper">
-            <a href="mailto:dineo06.moseki@gmail.com" class="logo" target="_blank"><icon name="email"></icon> Get in touch</a>
+            <a href="mailto:info@nellymoseki.co.za" class="logo" target="_blank"><icon name="email"></icon> Get in touch</a>
 
             <div class="menu desktop">
                 <ul>
                     <li><a href="https://www.linkedin.com/in/nelly-moseki-11a93170/" target="_blank">linkedIn</a>/</li>
-                    <li><a href="https://github.com/Paris2020/" target="_blank">Github</a>/</li>
+                    <li><a href="https://github.com/Paris2020" target="_blank">Github</a>/</li>
                     <li><a href="https://www.instagram.com/paris2020_/" target="_blank">Instagram</a></li>
                 </ul>
             </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -9,7 +9,7 @@
             <div class="projects-wrap">
                 <div class="project"><a href="https://help.afrihost.com/" target="_blank"><img src="/images/projects/help-centre.png" srcset="/images/projects/help-centre@2x.png" width="300" alt="Afrihost Help Centre"></a></div>
                 <div class="project"><a href="https://www.afrihost.com/voip/" target="_blank"><img src="/images/projects/pure-voip.png" srcset="/images/projects/pure-voip@2x.png" width="300" alt="Afrihost VoIP"></a></div>
-                <div class="project"><a href="https://www.afrihost.com/plus/" target="_blank"><img src="/images/projects/afrihost-plus.png" srcset="/images/projects/afrihost-plus@2x.png" width="300" alt="Afrihost plus"></a></div>
+                <div class="project"><a href="https://www.blog.afrihost.com/" target="_blank"><img src="/images/projects/afrihost-blog.png" srcset="/images/projects/afrihost-blogs@2x.png" width="300" alt="Afrihost blog page"></a></div>
                 <div class="project"><a href="https://www.openstreets.org.za/" target="_blank"><img src="/images/projects/openstreets.png" srcset="/images/projects/openstreets@2x.png" width="300" alt="Open Streets"></a></div>
                 <div class="project"><a href="https://www.bartgeier.ch/" target="_blank"><img src="/images/projects/bartgeier.png" srcset="/images/projects/bartgeier@2x.png" width="300" alt="Bartgeier"></a></div>
                 <div class="project"><a href="https://www.villageschoolsnamibia.com/" target="_blank"><img src="/images/projects/jd-village-school.png" srcset="/images/projects/jd-village-school@2x.png" width="300" alt="Village Schools Namibia"></a></div>
